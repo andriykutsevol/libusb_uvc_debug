dc1bafbd3ecc8fc205f7304bc4206d4c21ad4a11
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-20 18:24:30 +0200

    c

--- qemu_rebuild.sh
+++ qemu_rebuild.sh
@@ -7,7 +7,7 @@ qemu_src_path=${qemu_path}/src/qemu-6.0.0/
 
 
 cd ${qemu_src_path}
-git pull 
+git pull https://github.com/andriykutsevol/DGnet_Dist_PID3-my.git usbspoof_impr_spec1_working
 cd ${qemu_path}
 makepkg --skipinteg --skipchecksums --skippgpcheck -ef
 
