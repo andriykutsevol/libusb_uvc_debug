fc82104198879178caebe7f63a975b42a3be1c8d
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-21 17:15:52 +0200

    c

--- libuvc_build.sh
+++ libuvc_build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e 
-# ./libuvc_build.sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 
 #===================================
 # sudo apt update
 # sudo apt upgrade
 # sudo apt install libusb-1.0-0-dev
 # sudo apt install v4l-utils
+# sudo apt install qv4l2
+# sudo apt install v4l-conf
 # sudo apt install tcpdump
 # sudo apt install ffmpeg
 # sudo apt install vlc
+# sudo apt install mpv
 #===================================
-
 
 current_dir=`pwd`
 working_dir=${current_dir}"/libuvc_debug"
@@ -21,7 +28,7 @@ cd ${working_dir}
 #===================================
 
 rm -Rf ${build_dir}
-mkdir ${build_dir}
+mkdir -p ${build_dir}
 cd ${build_dir}
 #===================================
 
