fb8f36ca0093a4805b174cc68b8506a15943d626
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-26 16:36:39 +0200

    c

--- libusb_build.sh
+++ libusb_build.sh
@@ -43,19 +43,19 @@ make
 echo "=============================="
 echo "make install (to local dir): ${build_dir}/lib"
 echo "=============================="
-make install
+sudo make install
 
 echo "=============================="
 echo "install (cp, ln -s)"
 echo "=============================="
 
 
-rm -f /usr/lib/libusb-1.0.so.0.3.0
-rm -f /usr/lib/libusb-1.0.so.0
-rm -f /usr/lib/libusb-1.0.so
+sudo rm -f /usr/lib/libusb-1.0.so.0.3.0
+sudo rm -f /usr/lib/libusb-1.0.so.0
+sudo rm -f /usr/lib/libusb-1.0.so
 
-cp ${build_dir}/lib/libusb-1.0.so.0.3.0 /lib
-ln -s /usr/lib/libusb-1.0.so.0.3.0 /usr/lib/libusb-1.0.so.0
-ln -s /usr/lib/libusb-1.0.so.0.3.0 /usr/lib/libusb-1.0.so
+sudo cp ${build_dir}/lib/libusb-1.0.so.0.3.0 /lib
+sudo ln -s /usr/lib/libusb-1.0.so.0.3.0 /usr/lib/libusb-1.0.so.0
+sudo ln -s /usr/lib/libusb-1.0.so.0.3.0 /usr/lib/libusb-1.0.so
 
 
