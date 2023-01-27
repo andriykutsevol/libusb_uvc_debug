98153cb6c9e2a4745fb86880ff9a3b9a2beac60f
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-27 19:05:02 +0200

    c

--- make_slice.sh
+++ make_slice.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 #set -e
 
+if [ -z "$1" ]
+  then
+    echo "Two arguments required"
+    exit
+fi
+
+if [ -z "$2" ]
+  then
+    echo "Two arguments required"
+    exit
+fi
+
+
 working_dir=${1}
 target_dir=${2}
 
