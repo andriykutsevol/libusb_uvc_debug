f8e59c1728131d150a7265a6c6a46b513a97752a
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-28 00:25:57 +0200

    c

--- libuvc_example1_rally.sh
+++ libuvc_example1_rally.sh
@@ -37,11 +37,11 @@ output_dir=`pwd`"/rally/libuvc/${ex_num}"
 
 # ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1280 720 1 13 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1600 896 1 14 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1600 896 1 14 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1920 1080 1 15 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1920 1080 1 15 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 2560 1440 1 16 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 2560 1440 1 16 30 ${output_dir}
 
 ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 3840 2160 1 17 30 ${output_dir}
 
