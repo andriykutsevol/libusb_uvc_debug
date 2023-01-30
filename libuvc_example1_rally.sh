a09503cdbf89b2942c8d83def8a3047a3ca0a1a6
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-01-30 21:51:12 +0200

    c

--- libuvc_example1_rally.sh
+++ libuvc_example1_rally.sh
@@ -8,77 +8,77 @@ output_dir=`pwd`"/rally/libuvc/${ex_num}"
 
 # ffplay -f mjpeg -i ./out.mjpeg
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 480 0 0 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 480 0 0 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 160 120 0 1 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 160 120 0 1 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 176 144 0 2 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 176 144 0 2 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 180 0 3 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 180 0 3 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 240 0 4 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 240 0 4 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 352 288 0 5 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 352 288 0 5 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 480 270 0 6 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 480 270 0 6 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 360 0 7 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 360 0 7 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 448 0 8 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 448 0 8 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 600 0 9 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 600 0 9 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 848 480 0 10 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 848 480 0 10 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 960 540 0 11 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 960 540 0 11 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1024 576 0 12 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1024 576 0 12 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1280 720 0 13 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1280 720 0 13 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1600 896 0 14 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1600 896 0 14 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1920 1080 0 15 30 ${output_dir}
-
-
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1920 1080 0 15 30 ${output_dir}
 
 
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 480 1 0 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 160 120 1 1 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 176 144 1 2 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 480 1 0 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 180 1 3 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 160 120 1 1 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 240 1 4 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 176 144 1 2 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 352 288 1 5 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 180 1 3 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 480 270 1 6 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 320 240 1 4 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 360 1 7 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 352 288 1 5 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 448 1 8 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 480 270 1 6 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 600 1 9 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 640 360 1 7 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 848 480 1 10 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 448 1 8 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 960 540 1 11 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 800 600 1 9 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1024 576 1 12 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 848 480 1 10 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1280 720 1 13 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 960 540 1 11 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1600 896 1 14 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1024 576 1 12 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1920 1080 1 15 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1280 720 1 13 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 2560 1440 1 16 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1600 896 1 14 30 ${output_dir}
 
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 3840 2160 1 17 30 ${output_dir}
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 1920 1080 1 15 30 ${output_dir}
+
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 2560 1440 1 16 30 ${output_dir}
+
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 video0 3840 2160 1 17 30 ${output_dir}
 
 # ffprobe ./out
 # [mjpeg @ 0x564bed405900] Format mjpeg detected only with low score of 12, misdetection possible!
