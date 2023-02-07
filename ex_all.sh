2b22972ebeb7a7c646f9da4b743c376d9784d651
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-02-07 18:50:49 +0200

    c

--- ex_all.sh
+++ ex_all.sh
@@ -18,50 +18,50 @@ mkdir -p ${slices_dir}
 
 
 
-#===================================================================
-# libuvc_example1_google.sh
-#===================================================================
+# #===================================================================
+# # libuvc_example1_google.sh
+# #===================================================================
 
-device_dir=${working_dir}/google
-sudo rm -rf ${device_dir}/*
-
-
-ex_num=1
-output_dir=${device_dir}/libuvc/${ex_num}
+# device_dir=${working_dir}/google
+# sudo rm -rf ${device_dir}/*
 
 
-
-# sudo modprobe -r xhci_pci
-# sudo modprobe xhci_pci
-# v4l2-ctl --all --device /dev/${device_file}
-dev_file="video0"
+# ex_num=1
+# output_dir=${device_dir}/libuvc/${ex_num}
 
 
-# v4l2-ctl --device /dev/${dev_file} --list-formats-ext
+
+# # sudo modprobe -r xhci_pci
+# # sudo modprobe xhci_pci
+# # v4l2-ctl --all --device /dev/${device_file}
+# dev_file="video0"
 
 
-#v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=1920,height=1080,pixelformat=YUYV
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 1920 1080 0 0 30 ${output_dir}
-
-cd ${working_dir}
-#v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=1280,height=720,pixelformat=YUYV
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 1280 720 0 1 30 ${output_dir}
+# # v4l2-ctl --device /dev/${dev_file} --list-formats-ext
 
 
-cd ${working_dir}
-#v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=640,height=480,pixelformat=YUYV
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 640 480 0 0 30 ${output_dir}
+# #v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=1920,height=1080,pixelformat=YUYV
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 1920 1080 0 0 30 ${output_dir}
 
-#-----------------------
-#Make Slice
-#-----------------------
-cd ${working_dir}
-dev_name="google"
+# cd ${working_dir}
+# #v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=1280,height=720,pixelformat=YUYV
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 1280 720 0 1 30 ${output_dir}
 
-./make_slice.sh ${working_dir} ${dev_name}
-cp ./${dev_name}_slice.tar.gz ${slices_dir}/
-rm -rf ./${dev_name}_slice.tar.gz
-rm -rf ./${dev_name}_slice
+
+# cd ${working_dir}
+# #v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=640,height=480,pixelformat=YUYV
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 640 480 0 0 30 ${output_dir}
+
+# #-----------------------
+# #Make Slice
+# #-----------------------
+# cd ${working_dir}
+# dev_name="google"
+
+# ./make_slice.sh ${working_dir} ${dev_name}
+# cp ./${dev_name}_slice.tar.gz ${slices_dir}/
+# rm -rf ./${dev_name}_slice.tar.gz
+# rm -rf ./${dev_name}_slice
 
 
 
