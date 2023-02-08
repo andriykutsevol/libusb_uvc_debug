83687e07dac34fcc25aae56935ea3bab1312853c
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-02-08 23:27:02 +0200

    c

--- libuvc_example1_google.sh
+++ libuvc_example1_google.sh
@@ -33,3 +33,26 @@ cd ${working_dir}
 cd ${working_dir}
 #v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=640,height=480,pixelformat=YUYV
 ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 640 480 0 0 30 ${output_dir}
+
+
+
+
+
+
+#-----------------------
+#Make Slice
+#-----------------------
+working_dir=`pwd`
+cd ${working_dir}
+
+dev_name="google"
+slices_dir=${working_dir}/slices
+#sudo rm -rf ${slices_dir}/*
+sudo rm -rf {dev_name}_slice.tar.gz
+mkdir -p ${slices_dir}
+
+
+./make_slice.sh ${working_dir} ${dev_name}
+cp ./${dev_name}_slice.tar.gz ${slices_dir}/
+rm -rf ./${dev_name}_slice.tar.gz
+rm -rf ./${dev_name}_slice
