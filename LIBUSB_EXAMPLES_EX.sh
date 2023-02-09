9fe793fb98c7ed0ac4d255a294d2413a04cc7570
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-02-09 21:05:39 +0200

    c

--- LIBUSB_EXAMPLES_EX.sh
+++ LIBUSB_EXAMPLES_EX.sh
@@ -262,8 +262,8 @@ ${dwMaxPayloadTransferSize_mysetting} \
 ${endpoint_bytes_per_packet_mysetting} \
 ${bAlternateSetting_mysetting} \
 ${bEndpointAddres_mysetting} \
-${total_transfer_size_mysetting}# \
-#>> ${output_dir}/ex_out.txt 2>&1
+${total_transfer_size_mysetting} \
+>> ${output_dir}/ex_out.txt 2>&1
 
 
 cd ${output_dir}
