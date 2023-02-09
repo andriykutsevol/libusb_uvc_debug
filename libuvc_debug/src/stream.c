1b39a96ee7f700ec0554c9d5e3d77c4856228794
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-02-09 20:34:20 +0200

    c

--- libuvc_debug/src/stream.c
+++ libuvc_debug/src/stream.c
@@ -1633,6 +1633,7 @@ uvc_error_t uvc_stream_stop(uvc_stream_handle_t *strmh) {
   if (!strmh->running)
     return UVC_ERROR_INVALID_PARAM;
 
+  printf("UVCLIB: uvc_stop_streaming() 1\n");
   strmh->running = 0;
 
   pthread_mutex_lock(&strmh->cb_mutex);
@@ -1642,17 +1643,27 @@ uvc_error_t uvc_stream_stop(uvc_stream_handle_t *strmh) {
    */
   for(i=0; i < LIBUVC_NUM_TRANSFER_BUFS; i++) {
     if(strmh->transfers[i] != NULL)
+      printf("UVCLIB: uvc_stop_streaming() 2\n");
       libusb_cancel_transfer(strmh->transfers[i]);
   }
 
   /* Wait for transfers to complete/cancel */
   do {
+    printf("UVCLIB: uvc_stop_streaming() 3\n");
     for(i=0; i < LIBUVC_NUM_TRANSFER_BUFS; i++) {
-      if(strmh->transfers[i] != NULL)
+      printf("UVCLIB: uvc_stop_streaming() 4\n");
+      if(strmh->transfers[i] != NULL){
+        printf("UVCLIB: uvc_stop_streaming() 5\n");
         break;
+      }
     }
-    if(i == LIBUVC_NUM_TRANSFER_BUFS )
+    if(i == LIBUVC_NUM_TRANSFER_BUFS ){
+      printf("break z\n");
       break;
+    }
+
+    printf("UVCLIB: uvc_stop_streaming() 6 z\n"); 
+
     pthread_cond_wait(&strmh->cb_cond, &strmh->cb_mutex);
   } while(1);
   // Kick the user thread awake
@@ -1660,6 +1671,8 @@ uvc_error_t uvc_stream_stop(uvc_stream_handle_t *strmh) {
   pthread_mutex_unlock(&strmh->cb_mutex);
 
   /** @todo stop the actual stream, camera side? */
+
+  printf("UVCLIB: uvc_stop_streaming() 7\n");
 
   if (strmh->user_cb) {
     /* wait for the thread to stop (triggered by
