384a2004ea7ff02fcdc5e28d3a6f360880db7183
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-02-15 00:00:30 +0200

    c

--- libuvc_example1_google.sh
+++ libuvc_example1_google.sh
@@ -24,16 +24,17 @@ dev_file="video0"
 # v4l2-ctl --device /dev/${dev_file} --list-formats-ext
 
 
-cd ${working_dir}
-#v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=640,height=480,pixelformat=YUYV
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 640 480 0 0 30 ${output_dir}
+# cd ${working_dir}
+# #v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=640,height=480,pixelformat=YUYV
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 640 480 0 0 30 ${output_dir}
 
+cd ${working_dir}
 #v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=1920,height=1080,pixelformat=YUYV
 ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 1920 1080 0 0 30 ${output_dir}
 
-cd ${working_dir}
-#v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=1280,height=720,pixelformat=YUYV
-./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 1280 720 0 1 30 ${output_dir}
+# cd ${working_dir}
+# #v4l2-ctl --device /dev/${dev_file} --set-fmt-video=width=1280,height=720,pixelformat=YUYV
+# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 6353 20510 ${dev_file} 1280 720 0 1 30 ${output_dir}
 
 
 
