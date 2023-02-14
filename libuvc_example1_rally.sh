03eea58df215a443b07402dadd0bdfe5e3b06bed
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-02-14 17:20:13 +0200

    c

--- libuvc_example1_rally.sh
+++ libuvc_example1_rally.sh
@@ -21,29 +21,29 @@ dev_fname=video6
 
 ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 640 480 0 0 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 160 120 0 1 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 160 120 0 1 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 176 144 0 2 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 176 144 0 2 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 320 180 0 3 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 320 180 0 3 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 320 240 0 4 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 320 240 0 4 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 352 288 0 5 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 352 288 0 5 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 480 270 0 6 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 480 270 0 6 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 640 360 0 7 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 640 360 0 7 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 800 448 0 8 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 800 448 0 8 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 800 600 0 9 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 800 600 0 9 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 848 480 0 10 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 848 480 0 10 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 960 540 0 11 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 960 540 0 11 30 ${output_dir}
 
-# ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 1024 576 0 12 30 ${output_dir}
+./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 1024 576 0 12 30 ${output_dir}
 
 ./LIBUVC_EXAMPLES_EX.sh ${ex_num} 1133 2177 ${dev_fname} 1280 720 0 13 30 ${output_dir}
 
