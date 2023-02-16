739fb9c26cec16f73fcbd3a3186d82ef77b4d7e1
Andriy Kutsevol <andriykutsevol@gmail.com>
2023-02-16 17:12:50 +0200

    c

--- v4l2_example5_c270.sh
+++ v4l2_example5_c270.sh
@@ -8,9 +8,14 @@ fi
 
 
 output_dir=`pwd`"/c270/v4l2-examples/5"
+working_dir=`pwd`
 
-./v4l2_examples_ex.sh 5 1133 2085 video0 640 480 0 15 ${output_dir}
+dev_file="video0"
 
+./V4L2_EXAMPLES_EX.sh 5 6353 20510 ${dev_file} 1920 1080 0 -1 30 ${output_dir}
 
-./v4l2_examples_ex.sh 5 1133 2085 video0 1280 720 0 15 ${output_dir}
+cd ${working_dir}
+./V4L2_EXAMPLES_EX.sh 5 6353 20510 ${dev_file} 1280 720 0 -1 30 ${output_dir}
 
+# cd ${working_dir}
+# ./V4L2_EXAMPLES_EX.sh 5 6353 20510 video2 640 480 0 -1 60 ${output_dir}
